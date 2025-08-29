f01c197e0bda647bd48d4ee89cd6f3ed5e1f2802
NishadAnwar <mohammednishadanwar@gmail.com>
2025-08-30 02:07:44 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,249 @@
+export const Bio = {
+    name: "Md Nishad Anwar",
+    roles: [
+      "Full Stack Developer",
+      "Programmer",
+    ],
+    description: "To kickstart my career as a software developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization",
+    github: "https://github.com/NishadAnwar",
+    resume: "https://drive.google.com/file/d/1sqtkLDHDGOECHHlwBs0KjqOICzITOppH/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/md-nishad-anwar-7b4076224/",
+    twitter: "https://twitter.com/nishadanwar22",
+    insta: "https://www.instagram.com/nishaadanwar/",
+    facebook: "https://www.facebook.com/nishadanwar/",
+};
+  
+export const skills = [
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "C",
+          image: "C-logo.png",
+        },
+        {
+          name: "C++",
+          image: "CPP-logo.png",
+        },
+        {
+          name: "Java",
+          image: "java-logo.svg",
+        },
+        {
+          name: "Python",
+          image: "Python-logo.png",
+        },
+        {
+          name: "SQL",
+          image: "sql-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "html5-logo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "CSS3-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "bootstrap-logo.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "JavaScript-logo.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "react-logo.png",
+        },
+        {
+          name: "Tailwind CSS",
+          image:
+            "tailwind-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "nodejs-logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:
+            "Expressjs.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "mongoDB-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        // {
+        //   name: "Git",
+        //   image:
+        //     "Git-logo.png",
+        // },
+        {
+          name: "GitHub",
+          image:
+            "GitHub-logo.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "vscode-logo.png",
+        },
+      ],
+    },
+];
+  
+export const experiences = [
+    {
+      // id: 0,
+      // img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      // role: "DevOps & Fullstack Engineering Intern",
+      // company: "Flipr Inovations Pvt. Ltd.",
+      // date: "Aug 2023 - July 2023",
+      // desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      // skills: [
+      //   "Docker",
+      //   "Terraform",
+      //   "AWS",
+      //   "EC2",
+      //   "Portainer",
+      //   "Nginx",
+      //   "JavaScript",
+      //   "TypeScript",
+      //   "Node Js",
+      //   " Next Js",
+      // ],
+      // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+];
+  
+export const education = [
+    {
+      id: 0,
+      img: "cmrec-logo.jpg",
+      school: "CMR Engineering College, Hyderabad",
+      date: "2021 - 2025",
+      grade: "8.17 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at CMR Engineering College, Hyderabad with a CGPA of 8.17. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering(AI&ML)",
+    },
+    {
+      id: 1,
+      img: "SR-logo.png",
+      school: "SR Junior College, Warangal",
+      date: "2019 - 2021",
+      grade: "91.8 %",
+      desc: "I completed my Intermediate at SR Junior College, Warangal where I studied Maths, Physics and Chemistry stream.",
+      degree: "Board Of Intermediate, TS - MPC(Maths, Physics, Chemistry)",
+    },
+    {
+      id: 2,
+      img: "LLHS-logo.avif",
+      school: "Learners' Land High School, Warangal",
+      date: "2019",
+      grade: "9.5 GPA",
+      desc: "I completed my class 10th education at Learners' Land High School, Warangal",
+      degree: "Board Of Secondary Education, TS - SSC",
+    },
+];
+  
+export const projects = [
+
+  {
+      id: 9,
+      title: "Contact Management API",
+      description:
+        "A secure RESTful API for managing personal contacts with user authentication, built using Node.js, Express, MongoDB (Mongoose), and JWT for token-based authentication.",
+      image:
+        "Contact-API.png",
+      tags: [
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+      ],
+      category: "web api",
+      github: "https://github.com/NishadAnwar/Contact_Management-API",
+      webapp: "#",
+      
+    },
+    {
+      id: 9,
+      title: "Stock Price Predictor",
+      description:
+        "Built a web app predicting stock prices using ML models with integrated financial news sentiment analysis and Achieved 55% accuracy using XGBoost model; automated data ingestion using yFinance API.",
+      image:
+        "Stock Price Predictor.png",
+      tags: [
+        "Python",
+        "Machine Learning",
+        "StreamLit",
+        // "Pandas",
+        // "NumPy",
+        // "Matplotlib",
+        // "Scikit-learn",
+        // "yfinance"
+      ],
+      category: "web app",
+      github: "https://github.com/NishadAnwar/Stock-Price-Predictor",
+      webapp: "#",
+    },
+
+    {
+      id: 9,
+      title: "Portfolio",
+      description:
+        "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+      image:
+        "Portfolio.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "React",
+      ],
+      category: "web app",
+      github: "https://github.com/NishadAnwar/Nishad's Portfolio",
+      webapp: "#",
+    },
+
+    {
+      id: 9,
+      title: "Weather App",
+      description:
+        "Built a Weather App that is a web-based application that allows users to check the current weather conditions either based on their location or by searching for a city. It provides real-time weather updates using the OpenWeather API and features a user-friendly interface with a clean design.",
+      image:
+        "weatherApp.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "API",
+      ],
+      category: "web app",
+      github: "https://github.com/NishadAnwar/Weather-App",
+      webapp: "#",
+      
+    },
+];
+  
